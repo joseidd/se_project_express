56f3a11f1ae954c22414418c145948e4d3b138c7
joseiddd <joseidd@outlook.com>
2025-04-14 11:46:24 -0400

    new update

--- app.js
+++ app.js
@@ -15,14 +15,14 @@ mongoose
   .then()
   .catch(console.error);
 
-app.use(cors());
+app.use((req, res, next) => {
+  req.user = {
+    _id: "5d8b8592978f8bd833ca8133",
+  };
+  next();
+});
 
-if (process.env.NODE_ENV === "test") {
-  app.use((req, res, next) => {
-    req.user = { _id: "test-user-id" };
-    next();
-  });
-}
+app.use(cors());
 
 app.use(express.json());
 
