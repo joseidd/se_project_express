540e6be243639787cd030a1bfb876a973c6ecbbd
joseiddd <joseidd@outlook.com>
2025-04-14 11:23:02 -0400

    new update

--- app.js
+++ app.js
@@ -25,6 +25,13 @@ app.get("/crash-test", () => {
   }, 0);
 });
 
+app.use((req, res, next) => {
+  req.user = {
+    _id: "5d8b8592978f8bd833ca8133",
+  };
+  next();
+});
+
 app.use(requestLogger);
 
 app.use(routes);
