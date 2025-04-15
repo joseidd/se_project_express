08e77dbbc8a804eb5f188a3e96323408bd43bc80
joseiddd <joseidd@outlook.com>
2025-04-15 12:30:26 -0400

    new update

--- models/user.js
+++ models/user.js
@@ -1,6 +1,5 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
-const bcrypt = require("bcryptjs");
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -19,42 +18,6 @@ const userSchema = new mongoose.Schema({
       message: "You must enter a valid URL",
     },
   },
-  email: {
-    type: String,
-    required: [true, "The email Field is required."],
-    unique: true,
-    validate: {
-      validator(value) {
-        return validator.isEmail(value);
-      },
-      message: "You must enter valid email",
-    },
-  },
-  password: {
-    type: String,
-    required: [true, "The password is required."],
-    select: false,
-  },
 });
-
-userSchema.statics.findUserByCredentials = function findUserByCredentials(
-  email,
-  password
-) {
-  return this.findOne({ email })
-    .select("+password")
-    .then((user) => {
-      if (!user) {
-        return Promise.reject(new Error("Incorrect email or password"));
-      }
-
-      return bcrypt.compare(password, user.password).then((matched) => {
-        if (!matched) {
-          return Promise.reject(new Error("Incorrect email or password"));
-        }
-        return user;
-      });
-    });
-};
 
 module.exports = mongoose.model("user", userSchema);
--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,13 @@
 const router = require("express").Router();
-const NotFoundError = require("../customErrors/not-found-error");
+
 const userRouter = require("./users");
-const clothingRouter = require("./clothingItems");
+const clothingItems = require("./clothingItems");
 
+router.use("/items", clothingItems);
 router.use("/users", userRouter);
-router.use("/items", clothingRouter);
 
-router.use((req, res, next) => {
-  next(new NotFoundError("Route not found"));
+router.use((req, res) => {
+  res.status(404).send({ message: "Requested resource not found" });
 });
 
 module.exports = router;
