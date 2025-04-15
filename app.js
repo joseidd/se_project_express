87eb48673329c959e544de8ab9a2577f89ae5170
joseiddd <joseidd@outlook.com>
2025-04-15 08:47:46 -0400

    new update

--- app.js
+++ app.js
@@ -1,39 +1,27 @@
-require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
-const cors = require("cors");
-const { errors } = require("celebrate");
-const routes = require("./routes");
+const mainRouter = require("./routes/index");
 
 const app = express();
 const { PORT = 3001 } = process.env;
 
 mongoose
   .connect("mongodb://127.0.0.1:27017/wtwr_db")
-  .then()
+  .then(() => {
+    console.log("Connected to DB");
+  })
   .catch(console.error);
-
-app.use(cors());
-
-app.use(express.json());
 
 app.use((req, res, next) => {
   req.user = {
-    _id: "5d8b8592978f8bd833ca8133",
+    _id: "67f455406143076a1289fd72",
   };
   next();
 });
 
-// app.get("/crash-test", () => {
-//   setTimeout(() => {
-//     throw new Error("Server will crash now");
-//   }, 0);
-// });
-
-app.use(routes);
-
-app.use(errors());
+app.use(express.json());
+app.use("/", mainRouter);
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Listening on ${PORT}`);
 });
